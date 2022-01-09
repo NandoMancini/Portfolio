779c3fc81b0a9e40007e7f83cdb2f864b8ee362b
Fernando <fernandomancinidesousa@gmail.com>
2022-01-09 08:28:54 -0500

    Fixed Navber overline bug

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,9 +6,9 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div id="logo"><strong>Fernando S.</strong></div>
-            <div><a className="link">About Me</a></div>
-            <div><a className="link">Projects</a></div>
-            <div><a className="link">Contact</a></div>
+            <div><a id="link">About Me</a></div>
+            <div><a id="link">Projects</a></div>
+            <div><a id="link">Contact</a></div>
         </div>
     )
 }
