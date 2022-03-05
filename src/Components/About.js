64e5667a593994e13447c22d5c89af08dbc3b022
Fernando <fernandomancinidesousa@gmail.com>
2022-03-05 14:23:48 -0500

    Finished landing page

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,8 +3,11 @@ import "../Styles/About.css";
 
 const About = () => {
     return (
-        <div>
-            
+        <div className="about">
+            <div className="wrapper">
+                <h2>Hi, I'm</h2>
+                <h1>Fernando Sousa,</h1>
+            </div>
         </div>
     )
 }
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "../Styles/Hero.css"
 import { init } from "ityped"
 import { useEffect, useRef } from "react"
+import arrow from "../Assets/arrow.png"
 
 const Hero = () => {
 
@@ -13,16 +14,20 @@ const Hero = () => {
               backDelay: 1500,
               backSpeed: 50,
               
-              strings: ['coding.',  'problem solving.', 'cooking.', 'learning.'] })
+              strings: ['coding.',  'problem solving.', 'cooking.', 'learning.']}
+              )
     }, [])
 
     return (
-        <div className="body">
+        <div className="landing">
             <div className="wrapper">
                 <h2>Hi, I'm</h2>
                 <h1>Fernando Sousa,</h1>
                 <h3>and I enjoy <span ref={textRef}></span></h3>
                 <button>Contact Me.</button>
+            </div>
+            <div className="arrow">
+                <img src={arrow}/>
             </div>
         </div>
     )
