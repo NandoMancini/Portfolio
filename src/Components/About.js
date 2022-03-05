473ba59bb931223dd734f3adc6481817125d78ea
Fernando <fernandomancinidesousa@gmail.com>
2022-03-05 14:39:27 -0500

    Added layering with fixed position

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,10 +4,8 @@ import "../Styles/About.css";
 const About = () => {
     return (
         <div className="about">
-            <div className="wrapper">
-                <h2>Hi, I'm</h2>
-                <h1>Fernando Sousa,</h1>
-            </div>
+            <h2>Hi, I'm</h2>
+            <h1>Fernando Sousa,</h1>
         </div>
     )
 }
